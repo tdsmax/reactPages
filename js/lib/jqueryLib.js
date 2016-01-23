efaccf839def1ba000d2e77063a0652194d8a66e
tarandeep singh <tarandeep.singh@bonavitatech.com>
2016-01-24 00:18:21 +0530

    all other work

---
+++ js/lib/jqueryLib.js
@@ -0,0 +1,117 @@
+(function(global, document, undef) {
+
+    var strundef = typeof(undef),
+        strstring = typeof("");
+
+    var bonavita = global.Bonavita = global.Bonavita||{};
+
+    // Element factory
+    bonavita.bonavitaElement = function(elem) {
+        var ElemObj = {
+            hasClass: function(className) {
+                if (elem && typeof(className) === strstring) {
+                    var classAttr = elem.getAttribute("class");
+                    var classes = (classAttr) ? classAttr.split(" ") : [];
+                    return (classes.indexOf(className) > -1);
+                }
+                return false;
+            },
+            addClass: function(className) {
+                if (elem && typeof(className) === strstring && !this.hasClass(className)) {
+                    var classAttr = elem.getAttribute("class");
+                    var classes = (classAttr) ? classAttr.split(" ") : [];
+                    classes.push(className);
+                    this.attr("class", classes.join(" "));
+                }
+                return this;
+            },
+            removeClass: function(className) {
+                if (elem && typeof(className) === strstring && this.hasClass(className)) {
+                    var classAttr = elem.getAttribute("class");
+                    var classes = (classAttr) ? classAttr.split(" ") : [];
+                    var indexOfClass = classes.indexOf(className);
+                    while (indexOfClass > -1) {
+                        classes.splice(indexOfClass, 1);
+                        indexOfClass = classes.indexOf(className);
+                    }
+                    this.attr("class", classes.join(" "));
+                }
+                return this;
+            },
+            attr: function(attrName, attrValue) {
+                if (elem && typeof(attrName) === strstring && typeof(attrValue) === strundef) {
+                    return elem.getAttribute(attrName);
+                } else if (elem && typeof(attrName) === strstring && typeof(attrValue) !== strundef) {
+                    elem.setAttribute(attrName, attrValue);
+                    return attrValue;
+                }
+                return null;
+            },
+            bind: function(eventType, eventCallback, captureMode, data) {
+                if (elem && elem.addEventListener) {
+                    return (elem.addEventListener(eventType, function(evt) {
+                        evt = evt || window.event;
+                        eventCallback.call(this, evt, data);
+                    }, captureMode));
+                } else if (elem && elem.attachEvent) {
+                    return (elem.attachEvent("on" + eventType, function(evt) {
+                        evt = evt || window.event;
+                        eventCallback.call(elem, evt, data);
+                    }));
+                } else if (elem) {
+                    elem["on" + eventType] = eventCallback;
+                }
+            },
+            toggle: function() {
+                if (elem) {
+                    var disp = this.getStyle("display");
+                    elem.style.display = (disp === "block") ? "none" : "block";
+                    return (!(disp === "block"));
+                }
+            },
+            getStyle: function(styleAttr) {
+                if (elem) {
+                    if (typeof styleAttr === "string") {
+                        if (elem.currentStyle) {
+                            return elem.currentStyle[styleAttr];
+                        } else if (window.getComputedStyle) {
+                            return window.getComputedStyle(elem, null)[styleAttr];
+                        } else if (elem.style) {
+                            return elem.style[styleAttr];
+                        }
+                        return "";
+                    }
+                }
+                return "";
+            },
+            hide: function() {
+                if (elem) {
+                    elem.style.display = "none";
+                }
+                return this;
+            },
+            show: function() {
+                if (elem) {
+                    elem.style.display = "block";
+                }
+                return this;
+            },
+            parent: function() {
+                if (elem) {
+                    return elem.parentNode;
+                }
+                return null;
+            },
+            html: function (val) {
+                if (typeof (val) === strundef) {
+                    return elem.innerHTML;
+                } else {
+                    elem.innerHTML = val;
+                    return this;
+                }
+            }
+        };
+        return ElemObj;
+    };
+    
+}(window, document));
---
+++ js/lib/swapper.js
@@ -0,0 +1,74 @@
+/**
+ * @description This plugin is used to Swap Elements 
+ * 
+ * **/
+
+	/**
+	 * To use this plugin 
+	 * add a class hoverable on the child Elements and 
+	 * hoverableComponent class on the parent Container
+	 * to use this call Tds.swapper.moveUp(element) on a click method of Swapping Element
+	 * similarly call Tds.swapper.moveDown(element) to move down
+	 * you can also pass (this) it will figure out the element and work on the element with hoverable class. 
+	 * ***/
+
+(function(global, document, undef) {
+
+
+    var Tds = global.Tds = global.Tds||{};
+
+	var Elem = Tds.TdsElement;
+
+	var _moveDown = function(elem){
+		elem = elem.target ? elem.target : elem;
+		elem = Elem(elem).hasClass("hoverable") ? elem : elem.parentElement;
+		parentElem = elem.parentElement;
+		totalElem = parentElem.childElementCount - 1;
+		elemIndex = getChildElementdNumber(elem,parentElem);
+		nextElem = elemIndex ===  totalElem? elem : elem.nextElementSibling;
+		removeClasses(elem,nextElem);
+		Elem(elem).addClass("swapper");
+		parentElem.insertBefore(nextElem,elem);
+		animate(elem);
+	}
+
+	var _animate = function(elem){
+		setTimeout(function(){
+			Elem(elem).addClass("swapper-active");
+		}, 0);
+	}
+
+	var _removeClasses = function(elem,sibling){
+		if(Elem(elem).hasClass("swapper-active")){
+			Elem(elem).removeClass("swapper");
+			Elem(elem).removeClass("swapper-active");
+		}
+		if(Elem(sibling).hasClass("swapper-active")){
+			Elem(sibling).removeClass("swapper");
+			Elem(sibling).removeClass("swapper-active");
+		}
+	}
+
+	var _moveUp = function(elem){
+		elem = elem.target ? elem.target : elem;
+		elem = Elem(elem).hasClass("hoverable") ? elem : elem.parentElement;
+		parentElem = elem.parentElement;
+		elemIndex = getChildElementdNumber(elem,parentElem);
+		prevElem = elemIndex === 0 ? elem : elem.previousElementSibling;
+		removeClasses(elem,prevElem);
+		Elem(elem).addClass("swapper");
+		parentElem.insertBefore(elem,prevElem);
+		animate(elem);
+	}
+
+	var _getChildElementdNumber = function(element,parentElement) {
+	  return Array.prototype.indexOf.call(parentElement.children, element);
+	}
+	
+	
+	Tds.swapper = {
+		moveDown : _moveDown,
+		moveUp : _moveUp
+	}
+    
+}(window, document));
